eadea28505e2fdbdb3ce70a1e640d986f0b5062a
Kloska <tim.lundstrom@elev.ga.ntig.se>
2022-02-18 11:06:18 +0100

    routes, njk, css

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     charset: 'utf8mb4',
     host: process.env.DATABASE_HOST,
-    username: process.env.DATABASE_USER,
+    user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_DATABASE,
 });
--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../database');
+const pool = require('../../database');
 /* 
     BASE URL /tasks
     GET / - Get all tasks
@@ -12,36 +12,6 @@ const pool = require('../database');
 router.get('/', async (req, res, next) => {
     await pool.promise()
         .query('SELECT * FROM tasks')
-        .then(([rows, fields]) => {
-            res.render('tasks.njk',{
-                tasks: rows,
-                title: "Tasks",
-                layout: "layout.njk"
-
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                tasks: {
-                    error: 'Error getting tasks'
-                }
-            })
-        });
-});
-
-router.get('/:id', async (req, res, next) => {
-    const id = req.params.id;
-
-    if (isNaN(req.params.id)) {
-        res.status(400).json({
-            task: {
-                error: 'Bad request'
-            }
-        });
-    }
-    await pool.promise()
-        .query('SELECT * FROM tasks WHERE id = ?', [id])
         .then(([rows, fields]) => {
             res.json({
                 tasks: {
@@ -57,31 +27,92 @@ router.get('/:id', async (req, res, next) => {
                 }
             })
         });
-    // res.json({
-    //     id: req.params.id
-    // })
-
 });
 
-router.post('/', async (req, res, next) => {
-    const task = req.body.task;
-
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    if (isNaN(req.params.id)) {
+        res.status(400).json({
+            task: {
+                error: 'Bad request'
+            }
+        });
+    }
     await pool.promise()
-        .query('INSERT INTO tasks (task) VALUES (?)', [task])
-        .then((response) => {
+        .query('SELECT * FROM tasks WHERE id = ?', [id])
+        .then(([rows, fields]) => {
             res.json({
                 task: {
-                    data: response
+                    data: rows
                 }
             });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                tasks: {
+                task: {
                     error: 'Error getting tasks'
                 }
             })
         });
 });
+
+router.get('/:id/delete', async (req, res, next) => {
+    const id = req.params.id;
+    res.json(`deleting task ${id}`);
+    // if (isNaN(req.params.id)) {
+    //     res.status(400).json({
+    //         task: {
+    //             error: 'Bad request'
+    //         }
+    //     });
+    // }
+});
+
+router.post('/', async (req, res, next) => {
+    // { "task": "koda post" }
+    const task = req.body.task;
+    await pool.promise()
+    .query('INSERT INTO tasks (task) VALUES (?)', [task])
+    .then((response) => {
+        res.json({
+            task: {
+                data: response
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            task: {
+                error: 'Error getting tasks'
+            }
+        })
+    });
+    
+    
+    // res.json(req.body);
+
+});
+
+
 module.exports = router;
+
+
+
+/*
+    await pool
+    .promise()
+    .query('SELECT * FROM users')
+    .then(([rows, fields]) => {
+        res.json({
+            data: rows,
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+            error: 'Database error',
+        });
+    });
+    */
