e83d25ff26086b6548b3f1afd49995b577e76e63
Kloska <tim.lundstrom@elev.ga.ntig.se>
2022-02-28 11:07:23 +0100

    Delete button

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
@@ -75,15 +76,48 @@ router.post('/', async (req, res, next) => {
             } else {
                 res(400)
             }
-        })    
+        })
 
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            tasks: {
-                error: 'Error getting tasks'
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: 'Error getting tasks'
+                }
+            })
+        });
+});
+
+router.get('/:id/delete', async (req, res, next) => {
+    const id = req.params.id;
+    if (isNaN(req.params.id)) {
+        res.status(400).json({
+            task: {
+                error: 'Bad request'
+            }
+        });
+    }
+    await pool.promise()
+        .query('DELETE FROM tasks WHERE id = ?', [id])
+        .then((response) => {
+            console.log(response[0].affectedRows);
+            if (response[0].affectedRows === 1) {
+                res.redirect('/tasks');
+            } else {
+                res.status(400).json({
+                    task: {
+                        error: 'Invalid task'
+                    }
+                });
             }
         })
-    });
-});
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                task: {
+                    error: 'Error getting tasks'
+                }
+            })
+        });
+})
 module.exports = router;
